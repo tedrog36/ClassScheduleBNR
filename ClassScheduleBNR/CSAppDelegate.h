315331fca534368ee178bc4c4b4a09e2d36830ce
Ted Rogers <tedrog36@gmail.com>
2013-03-12 11:53:46 -0400

    Initial Commit

---
+++ ClassScheduleBNR/CSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSAppDelegate.h
+//  ClassScheduleBNR
+//
+//  Created by Ted Rogers on 3/12/13.
+//  Copyright (c) 2013 Ted Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
